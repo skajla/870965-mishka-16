b6ce7cd8e113f6b5f7ce61516f43b04d2327713f
Виктория <viktoriya_2306@mail.ru>
2019-05-08 21:02:13 +0300

    task8

--- gulpfile.js
+++ gulpfile.js
@@ -35,3 +35,95 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+
+
+
+// var gulp = require('gulp');
+// var imagemin = require('gulp-imagemin');
+// var cheerio = require('gulp-cheerio');
+
+// gulp.task('images', function() {
+//   return gulp.src('source/img/**/*.{jpg,svg}')
+//     .pipe(imagemin([
+//       imagemin.jpegtran({progressive: true}),
+//       imagemin.svgo()
+//     ]))
+//     .pipe(cheerio({
+//       run: function ($) {
+//         $('[fill]').removeAttr('fill');
+//         $('[stroke]').removeAttr('stroke');
+//         $('[style]').removeAttr('style');
+//       },
+//       parserOptions: {xmlMode: true}
+//     }))
+//     .pipe(gulp.dest('source/img'));
+// });
+
+
+
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('images', function() {
+  return gulp.src('source/img/**/*.{jpg,svg}')
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('source/img'));
+});
+
+
+
+var gulp = require('gulp');
+var cheerio = require('gulp-cheerio');
+var replace = require('gulp-replace');
+
+gulp.task('fillDelete', function() {
+  return gulp.src('source/img/icon-*.svg')
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: {xmlMode: true}
+    }))
+    .pipe(replace('&gt;', '>'))
+    .pipe(gulp.dest('source/img'));
+});
+
+
+
+
+
+
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img/"));
+});
+
+
+
+
+var gulp = require('gulp');
+var svgFill = require('gulp-svg-fill');
+
+gulp.task('colorSvgs', function() {
+  return gulp.src('source/img/icon-feature-*.svg')
+    .pipe(svgFill({
+      colors: {
+        'seafoam-blue': '#63d1bb',
+      }
+    }))
+    .pipe(gulp.dest('source/img'));
+});
